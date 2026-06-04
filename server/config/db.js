eb1b692b50a2ad4fa6e58bc7d814d341061367e7
Sandeep Morchhale <sandeepmorchhale2002@gmail.com>
2026-06-04 17:53:09 +0530

    mongodb bug fix

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://sandeep77249_db_user:CaT6q1EW8BFAkw03@cluster0.2gg2dyi.mongodb.net/Farming_Taknik?appName=Cluster0')
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb server error')
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
